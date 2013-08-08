7993a19c3cfc5f9f2ecee47cffd7e19c9737efe9
Chadwick Flores <chadwickflores@gmail.com>
2013-08-07 20:03:05 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rndzvous::Application.config.secret_token = 'ec11cdfe2d2693e70c196af76cf4418986188b9f128ef9adf8735060533d6990813b7fa8b52f48f684cb2d0ee095e3bd9f06ff604f475de28380078928b99b8e'
