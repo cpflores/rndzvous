871670cc4c30fcaa67a68d24c060e849a18d407f
Chadwick Flores <chadwickflores@gmail.com>
2013-09-17 18:54:03 -0500

    Added debugger in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
-
+    puts "events: <#{@events}>"
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @events }
